be466921f19f58ff4bc421d74b35c9cb12c5b522
Nikitha <nikitha.pechieswaran@gmail.com>
2025-03-21 12:29:17 +0530

    patient registration successful

--- wellness-backend/config/config.js
+++ wellness-backend/config/config.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
   PORT: process.env.PORT || 3001, // Change from 3000 to 3001
   MONGO_URI: process.env.MONGODB_URI,
-  JWT_SECRET: process.env.JWT_SECRET || '4371056fabcb0502e5f9fdcb5e4ba943ee5a0d16a2bcbc55289e95448bfe58f8af5e1e43c0369095b01ddfb4a7',
+  JWT_SECRET: process.env.JWT_SECRET || 'a8c87dfab0c5326f0776610ed5dee0ccbaa544ad5c831a83fc3b029781c65dda8bfce5ca5aca573f095bd86a17781804e7648f7a66270d9f2b79b8c1190bfbe5',
   CORS_ORIGIN: process.env.CORS_ORIGIN || 'http://localhost:5174' // Update to match your frontend port
 };
